d7cbc1e82ed9489ae359c2f0ed1dd3d028996ff8
Lucas Manhães <ramosmanhaes@outlook.com>
2025-02-27 02:51:56 -0300

    Adicionando Host Remoto

--- src/services/service.ts
+++ src/services/service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: "https://fitness-pe70.onrender.com"
 })
 
 export const login = async (url: string, dados: Object, setDados: Function) => {
